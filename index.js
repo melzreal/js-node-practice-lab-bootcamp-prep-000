6a39e02963f682f83a36cd361e6a7b9291c0a737
Melissa Real <melissa.real@hotmail.com>
2018-01-08 16:33:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Mel"
-var height = "74"
+var height = "1.70"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)+1)
